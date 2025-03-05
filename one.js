6b18fb6b20969e676f7716dc8d59619add65d721
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-05 17:57:20 +0600

    add user input functionality and update HTML structure

--- one.js
+++ one.js
@@ -1,42 +1,19 @@
-let students=30;
-students=students+10;
-console.log(students);
+/*
+How to take user input
+   1.easy way=window.prompt("Enter your name");
+   2.professional way=html textboxes
+How to print output
+   1.console.log("Hello World");
+   2.alert("Hello World");
+   3.document.write("Hello World");
+*/
+// let user;
+// user=window.prompt("Enter your name");
+// console.log("Hello "+user);
 
-students--;
-console.log(students);
-
-students=students*2;
-console.log(students);
-
-students=students/2;
-console.log(students);
-
-students=students**3; // students^3
-console.log(students);
-
-let extra_students=students%2;
-console.log(extra_students);
-
-
-
-// augmented operators
-students+=10;
-students-=5;
-students*=2;
-students/=2;
-students**=3;
-extra_students%=2;
-
-
-// operator precedence
-//    1. ()
-//     2. **
-//     3. * / %
-//     4. + -
-//     5. = +=
-//     6. ++ --
-
-
-
-let x=10+5*2+2**3/4;
-console.log(x); 
+//By using html textboxes
+let username;
+document.getElementById("mysub").onclick=function(){
+username=document.getElementById("mytext").value;
+document.getElementById("myh1").textContent=`Yoooo ${username} !!!`;
+};
