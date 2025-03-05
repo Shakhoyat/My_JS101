2e7efe72f7bd8a05719a804b1d0206d01462ce10
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-05 16:09:54 +0600

    feat: add new variables and enhance logging in one.js

--- one.js
+++ one.js
@@ -1,5 +1,13 @@
 //declaration of variable
 let a = 10;
 a=150;
+let price=10.99;
+let name="John";
+let age=25;
 
-console.log(a);
+console.log(price);
+console.log(`The price is ${price}`);
+
+console.log(`You are a ${age} year old boy, ${name}`);
+
+console.log(name);
