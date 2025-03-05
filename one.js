3d9f7ae3d4011a6750bc9088da8625b0651b2b44
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-05 17:02:56 +0600

    feat: update HTML structure and enhance JavaScript output with new variables

--- one.js
+++ one.js
@@ -1,26 +1,7 @@
-//declaration of variable
-let a = 10;
-a=150;
-let price=10.99;
-let name="John";
+let fullname="John Doe";
 let age=25;
+let is_student=true;
 
-console.log(price);
-console.log(`The price is ${price}`);
-
-console.log(`You are a ${age} year old boy, ${name}`);
-
-console.log(name);
-
-console.log(typeof name);
-
-let favFood="Pizza";
-console.log(`you like ${favFood}`);
-
-let onlinestatus=true;
-console.log(onlinestatus);
-console.log(typeof onlinestatus);
-
-let for_sale=false;
-console.log(`Is this car for sale : ${for_sale}`);
-
+document.getElementById("p1").textContent=`Your fullname is ${fullname}`;
+document.getElementById("p2").textContent=`Your age is ${age}`;
+document.getElementById("p3").textContent=`You're a student :${is_student}`;
