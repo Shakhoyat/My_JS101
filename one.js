f15b4f17d08b80d2cafb3b2bfba670f04428d4b6
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-05 16:03:42 +0600

    editing txt using js

--- one.js
+++ one.js
@@ -1,5 +1,7 @@
-console.log(`HELLOOOOOOOO!!!!`);
-console.log(`I like Mangoes`);
+// console.log(`HELLOOOOOOOO!!!!`);
+// console.log(`I like Mangoes`);
 
-// window.alert(`I am an alert box!`);
-window.alert(`I am an alert pizza!`);
+// // window.alert(`I am an alert box!`);
+// window.alert(`I am an alert pizza!`);
+document.getElementById("my_h1").textContent=`I am a changed h1!`;
+document.getElementById("my_p").textContent=`I am a changed p!`;
