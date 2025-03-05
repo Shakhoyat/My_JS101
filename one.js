bd00a7742f14719f6ce0c69e631046879937f953
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-05 23:21:46 +0600

    converting  string to a number

--- one.js
+++ one.js
@@ -1,19 +1,6 @@
-/*
-How to take user input
-   1.easy way=window.prompt("Enter your name");
-   2.professional way=html textboxes
-How to print output
-   1.console.log("Hello World");
-   2.alert("Hello World");
-   3.document.write("Hello World");
-*/
-// let user;
-// user=window.prompt("Enter your name");
-// console.log("Hello "+user);
+//Type conversion
 
-//By using html textboxes
-let username;
-document.getElementById("mysub").onclick=function(){
-username=document.getElementById("mytext").value;
-document.getElementById("myh1").textContent=`Yoooo ${username} !!!`;
-};
+let age=window.prompt("Enter your age");
+ age=Number(age);
+age+=1;
+console.log(age,typeof age);
