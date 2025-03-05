77df59053adb1bd4b9771dbb4304ad9784ae9f2e
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-05 17:29:45 +0600

    arithmatic operators

--- one.js
+++ one.js
@@ -1,7 +1,40 @@
-let fullname="John Doe";
-let age=25;
-let is_student=true;
+let students=30;
+students=students+10;
+console.log(students);
 
-document.getElementById("p1").textContent=`Your fullname is ${fullname}`;
-document.getElementById("p2").textContent=`Your age is ${age}`;
-document.getElementById("p3").textContent=`You're a student :${is_student}`;
+students--;
+console.log(students);
+
+students=students*2;
+console.log(students);
+
+students=students/2;
+console.log(students);
+
+students=students**3; // students^3
+console.log(students);
+
+let extra_students=students%2;
+console.log(extra_students);
+
+
+students+=10;
+students-=5;
+students*=2;
+students/=2;
+students**=3;
+extra_students%=2;
+
+
+// operator precedence
+//    1. ()
+//     2. **
+//     3. * / %
+//     4. + -
+//     5. = +=
+//     6. ++ --
+
+
+
+let x=10+5*2+2**3/4;
+console.log(x); 
