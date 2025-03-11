5b7236a9d1ad9908fafc376a197f3ef8be6525d4
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-12 01:43:34 +0600

    implement age eligibility check with user input and display result

--- one.js
+++ one.js
@@ -1,10 +1,19 @@
 //if else statement
 
-let age = prompt("Please enter your age:");
-age = parseInt(age);
-if (age >= 18) {
-  console.log("You are eligible to vote");
-}
-else {
-  console.log("You are not eligible to vote");
+const myip=document.getElementById('myip');
+const mybtn=document.getElementById('mybtn');
+const mylbl=document.getElementById('mylbl');
+const resultElement=document.getElementById('resultElement');
+
+let age;
+
+mybtn.onclick=function(){
+
+   age=Number(myip.value);
+   if(age>=18){
+       resultElement.textContent="You are eligible to vote";
+   }
+   else{
+       resultElement.textContent="You are not eligible to vote";
+   }
 }
