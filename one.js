cf07a287913e68c781ab4f08bcfafd2156141e1a
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-12 01:09:09 +0600

    random number generation in one.js

--- one.js
+++ one.js
@@ -1,22 +1,4 @@
-//Math=builtin object that provides math functions
-//Math.PI=3.141592653589793
-//Math.E=2.718281828459045
-//Math.LN2=0.6931471805599453
+//random number generator
 
-console.log(Math.PI);
-console.log(Math.E);
-console.log(Math.LN2);
-console.log(Math.LN10);
-console.log(Math.LOG2E);
-
-let x=45
-
-console.log(Math.pow(2,3));
-let z=Math.log(x);
-console.log(z);
-console.log(Math.tan(x));
-console.log(Math.sign(-3));
-let max=Math.max(1,2,3,4,5,6,7,8,9,10);
-console.log(max);
-let min=Math.min(1,2,3,4,5,6,7,8,9,10);
-console.log(min);
+let randomNumber = Math.floor(Math.random()*6)+1;
+console.log(randomNumber);
