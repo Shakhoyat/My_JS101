df3567e69c338bfca9832b03e6e1f47d086de49c
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-13 02:49:49 +0600

    Temperature Conversion Program

--- one.js
+++ one.js
@@ -1,13 +1,21 @@
-//function in js
+const textBox = document.getElementById("textbox");
+const tocelsius = document.getElementById("tocelsius");
+const toFarenheit = document.getElementById("toFarenheit");
+const result = document.getElementById("result");
+// const textBox = document.getElementById("textbox");
 
-function hbd() {
-    for (let i = 0; i < 5; i++) {
-        console.log("Happy Birthday");
-    }
-}
-hbd();
+let temp;
 
-function isEven(n) {
-    return n % 2 === 0;
+function convert() {
+  if (tocelsius.checked) {
+    temp = Number(textBox.value);
+    temp = ((temp - 32) * 5) / 9;
+    result.textContent = temp.toFixed(2) + ` Celsius`;
+  } else if (toFarenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(2) + ` Farenheit`;
+  } else {
+    result.textContent = "Please select a conversion type";
+  }
 }
-console.log(isEven(5));
