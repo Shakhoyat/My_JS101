c67540bd62fcf579376ce8d1c2b80384397729de
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-12 21:43:26 +0600

    function

--- one.js
+++ one.js
@@ -1,39 +1,13 @@
-//number guessing game
+//function in js
 
-
-const min = 1;
-const max = 100;
-const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-
-
-let attempts = 0;
-let guess;
-let running = true;
-
-while (running) {
-
-
-    guess = window.prompt(`Guess a number between ${min} and ${max}`);
-    guess = Number(guess);
-
-    if (isNaN(guess)) {
-        window.alert('Please enter a valid number');
-        continue;
-    } else if (guess < min || guess > max) {
-        window.alert(`Please enter a number between ${min} and ${max}`);
-        continue;
-    } else {
-        if (guess > randomNumber) {
-            window.alert('Too high');
-            attempts++;
-        } else if (guess < randomNumber) {
-            window.alert('Too low');
-            attempts++;
-        } else {
-            attempts++;
-            window.alert(`Congratulations! You guessed the number in ${attempts} attempts`);
-            running = false;
-        }
+function hbd() {
+    for (let i = 0; i < 5; i++) {
+        console.log("Happy Birthday");
     }
-
 }
+hbd();
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+console.log(isEven(5));
