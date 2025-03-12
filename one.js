108d190791bf5fea411a9e66dfb26aa23858de07
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-12 16:05:35 +0600

    ternary

--- one.js
+++ one.js
@@ -1,26 +1,5 @@
-//checkbox &radiobutton
-const mycheckbox = document.getElementById('toggle');
-const visabtn = document.getElementById('visabtn');
-const paypalbtn = document.getElementById('paypalbtn');
-const masterbtn = document.getElementById('masterbtn');
-const mysub = document.getElementById('mysub');
-const subres = document.getElementById('subres');
-const paymentres = document.getElementById('paymentres');
+//ternary operator
 
-
-mysub.onclick = function () {
-    if (mycheckbox.checked) {
-        subres.textContent = `You're Subscribed`;
-    } else {
-        subres.innerHTML = `You're not Subscribed`;
-    }
-
-    if(visabtn.checked){
-        paymentres.textContent = `You've selected Visa`;}
-    else if(paypalbtn.checked){
-        paymentres.textContent = `You've selected Paypal`;}
-    else if(masterbtn.checked){
-        paymentres.textContent = `You've selected Mastercard`;}
-    else{
-    paymentres.textContent = `You've not selected any payment method`;}
-}
+let time=19;
+let greeting = time < 18 ? "Good day" : "Good evening";
+console.log(greeting);
