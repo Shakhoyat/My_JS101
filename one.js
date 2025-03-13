0f76dc7ec9bb203eccf265a0e1ad64b26235b51d
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-13 16:26:19 +0600

    rest parameters

--- one.js
+++ one.js
@@ -1,20 +1,47 @@
-//spread operator is used to copy the object and not the reference
+//rest parameters in js
 
-let numbers = [10, -2, 3, 4];
+//rest is opposite of spread operator
+//spread operator is used to spread the elements of an array or object
+//rest is used to collect multiple elements and put them into a single array
 
-let maxi = Math.max(...numbers);
-let mini = Math.min(...numbers);
-console.log(maxi);
-console.log(mini);
+// const food1 = "pizza";
+// const food2 = "burger";
+// const food3 = "icecream";
 
-let user = "John";
-let user2 = [...user].join("-");
-console.log(user2);
+// //if we want to collect all the food items in a single array
+// function collectFoods(...foods) {
+//   console.log(...foods);
+// }
+// collectFoods(food1, food2, food3);
 
-let fruits = ["Apple", "Banana", "Orange"];
-let vegetables = ["Carrot", "Potato", "Cabbage"];
-//shallo copy means it will copy the references in a different data structure
-
-let foods = [...fruits, ...vegetables, "eggs", "milk"];
-
+const food1 = "pizza";
+const food2 = "burger";
+const food3 = "icecream";
+const food4 = "chocolate";
+const food5 = "cake";
+const food6 = "donut";
+const food7 = "pasta";
+const food8 = "noodles";
+const food9 = "sandwich";
+function openfridge(...foods) {
+  console.log(...foods);
+}
+openfridge(food1, food2, food3, food4, food5, food6, food7, food8, food9);
+//output: pizza burger icecream chocolate cake donut pasta noodles sandwich
+//all the food items are collected in a single array
+function getfood(...foods) {
+  return foods;
+}
+const foods = getfood(
+  food1,
+  food2,
+  food3,
+  food4,
+  food5,
+  food6,
+  food7,
+  food8,
+  food9
+);
 console.log(foods);
+//output: ["pizza", "burger", "icecream", "chocolate", "cake", "donut", "pasta", "noodles", "sandwich"]
