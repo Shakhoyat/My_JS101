bd0d96dfc458290056e183563a9f61aa9aec9e9d
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-13 16:33:13 +0600

    use case for strings

--- one.js
+++ one.js
@@ -21,3 +21,9 @@ function getAvg(...numbers) {
 
 const avg = getAvg(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The average is ${avg}`); // The average is 5.5
+
+function combineStrings(...strings) {
+  return strings.join(" ");
+}
+const fullname = combineStrings("John", "Doe");
+console.log(`The full name is ${fullname}`); // The full name is John Doe
