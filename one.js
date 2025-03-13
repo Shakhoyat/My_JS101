8f9b0643b470acb4b6b872ed48b683f97ca6ab74
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-14 05:46:03 +0600

    forEach() usecase

--- one.js
+++ one.js
@@ -1,14 +1,22 @@
-//callback is a function that is passed as an argument to another function and is executed after some operation has been completed
+//for each() method
 
-function sum(a, b, callback) {
-  callback(a + b);
-}
-function display_func(result) {
-  console.log(result);
-}
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(double);
+arr.forEach(square);
+arr.forEach(display); //array.forEach(callbackFunction) means element ,index ,array are provided
 
-function displayPage(result) {
-  document.getElementById("myh1").textContent = result;
+function double(item, index, array) {
+  array[index] = item * 2;
 }
 
-sum(5, 6, displayPage); //this means when sum is done, displayPage will be called with the result of sum
+function triple(item, index, array) {
+  array[index] = item * 3;
+}
+
+function square(item, index, array) {
+  array[index] = item * item;
+}
+
+function display(item) {
+  console.log(item);
+}
