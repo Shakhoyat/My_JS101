8121127b6ffaedb7151493b6cc5015e0fb0f2cd9
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-14 05:37:07 +0600

    callback is joss

--- one.js
+++ one.js
@@ -1,14 +1,14 @@
 //callback is a function that is passed as an argument to another function and is executed after some operation has been completed
 
-function hello(callback) {
-  console.log("This is hello");
-  callback();
+function sum(a, b, callback) {
+  callback(a + b);
 }
-function goodbye() {
-  console.log("This is goodbye");
+function display_func(result) {
+  console.log(result);
 }
-function leave() {
-  console.log("leave!!!");
+
+function displayPage(result) {
+  document.getElementById("myh1").textContent = result;
 }
-// hello(goodbye);
-hello(leave);
+
+sum(5, 6, displayPage); //this means when sum is done, displayPage will be called with the result of sum
