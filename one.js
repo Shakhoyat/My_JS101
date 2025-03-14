8a422e0307acb3b9d1560be5beacdec38c61cd09
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-14 08:36:04 +0600

    classes

--- one.js
+++ one.js
@@ -1,27 +1,20 @@
-//constructor
-
-function car(brand, model, year, color) {
-  this.brand = brand;
-  this.model = model;
-  this.year = year;
-  this.color = color;
-
-  this.drive = function () {
-    console.log("driving");
-  };
-  this.stop = function () {
-    console.log("stop");
-  };
-  this.getCarInfo = function () {
-    console.log(
-      `Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}, Color: ${this.color}`
-    );
-  };
+//classes
+class product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  display() {
+    console.log(this.name + " product is priced as : " + this.price);
+  }
+  calcTotal(salesTax) {
+    return this.price + this.price * salesTax;
+  }
 }
 
-const car1 = new car("BMW", "X5", 2020, "black");
-const car2 = new car("Audi", "A6", 2019, "white");
-console.log(car1.brand); // BMW
-console.log(car2.brand); // Audi
-car1.drive(); // driving
-car2.getCarInfo(); // driving
+const product1 = new product("Laptop", 1000);
+product1.display();
+const salesTax = 0.08;
+
+const total = product1.calcTotal(salesTax);
+console.log(total);
