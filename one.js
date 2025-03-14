c78de46099516d252f140b98cc8a7d9a1a47a583
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-15 01:24:03 +0600

    array of objects

--- one.js
+++ one.js
@@ -1,24 +1,49 @@
-//nested objects=> objects inside of other objects
-class Address {
-  constructor(city, street, zip) {
-    this.city = city;
-    this.street = street;
-    this.zip = zip;
-  }
-}
-class person {
-  constructor(name, age, ...address) {
-    this.name = name;
-    this.age = age;
-    this.address = new Address(...address);
-  }
-}
+//Array of objects
 
-const person1 = new person("John", 30, "New York", "Wall Street", 10005);
-// console.log(person1);
+const fruits = [
+  { name: "apple", color: "red", calories: 95 },
+  { name: "banana", color: "yellow", calories: 105 },
+  { name: "grape", color: "purple", calories: 62 },
+  { name: "kiwi", color: "brown", calories: 42 },
+  { name: "orange", color: "orange", calories: 62 },
+  { name: "pear", color: "green", calories: 57 },
+  { name: "strawberry", color: "red", calories: 4 },
+  { name: "watermelon", color: "green", calories: 46 },
+];
 
-const person2 = new person("Jane", 25, "California", "Sunset Blvd", 90001);
-// console.log(person2);
+// console.log(fruits[0].name);
+// console.log(fruits[0].color);
+// console.log(fruits[4].calories);
 
-const person3 = new person("Doe", 35, "Texas", "Main Street", 75001);
-console.log(person3.address.street);
+fruits.push({ name: "pineapple", color: "yellow", calories: 83 });
+fruits.pop();
+console.log(fruits);
+fruits.shift(); //remove from the beginning
+console.log(fruits);
+fruits.unshift({ name: "mango", color: "yellow", calories: 202 }); //add to the beginning
+console.log(fruits);
+fruits.splice(2, 1); //remove 1 element from index 2
+console.log(fruits);
+
+//for each method
+
+fruits.forEach((fruit) => {
+  console.log(fruit.color);
+});
+
+//map method
+
+const fruitNames = fruits.map((fruit) => fruit.name); //return an array of fruit names
+console.log(fruitNames);
+
+//filter method
+
+const redFruits = fruits.filter((fruit) => fruit.color === "red");
+console.log(redFruits);
+
+//reduce method
+
+const maxFruit = fruits.reduce((maxFruit, fruit) =>
+  fruit.calories > maxFruit.calories ? fruit : maxFruit
+);
+console.log(maxFruit.name);
