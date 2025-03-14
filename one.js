ff711415ae6fc9a27930be013de1201b67bb8b15
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-15 01:02:05 +0600

    nested objects

--- one.js
+++ one.js
@@ -1,56 +1,23 @@
-//destructuring = extract values from arrays and objects ,then assign them to variables in a single line of code
-//[]=to perform array destructuring
-//{}=to perform object destructuring
+//nested objects=> objects inside of other objects
 
-//swapping values of two variables
-let a = 1;
-let b = 2;
-
-[a, b] = [b, a];
-
-console.log(a);
-console.log(b);
-//swapping to elements of an array
-let arr = [1, 2, 3, 4];
-[arr[0], arr[1]] = [arr[1], arr[0]];
-console.log(arr);
-
-//assigning elements of an array to variables
-const arr2 = [1, 2, 3, 4, 104, 110, 112];
-const [a1, b1, c1, d1, ...extra] = arr2;
-console.log(a1);
-console.log(b1);
-console.log(extra);
-
-//extracting values from obj
-
-const person1 = {
-  firstname: "skt",
-  lastname: "rahman",
-  age: 39,
-  job: "Fry Cook",
+const person = {
+  fullname: "John Doe",
+  age: 25,
+  isStudent: false,
+  hobbies: ["reading", "coding", "swimming"],
+  address: {
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zip: "10001",
+  },
 };
-const person2 = {
-  firstname: "sohan",
-  lastname: "islam",
-  age: 34,
-  job: "Army",
-};
-const { firstname, lastname, age, job } = person1;
+console.log(person.fullname);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.hobbies);
+console.log(person.address.city);
 
-// console.log(firstname);
-// console.log(lastname);
-// console.log(age);
-// console.log(job);
-
-const { firstname: fn, lastname: ln, age: ag, jobz = "unemployed" } = person2;
-console.log(fn);
-
-//destructuring in function parameters
-function printPerson({ firstname, lastname, age, job }) {
-  console.log(firstname);
-  console.log(lastname);
-  console.log(age);
-  console.log(job);
+for (const property in person.address) {
+  console.log(`${property}: ${person.address[property]}`);
 }
-printPerson(person1);
