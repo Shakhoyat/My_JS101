9a5100508c9479bcd0691a69a690ea68d1d4295d
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-14 20:30:17 +0600

    inheritance

--- one.js
+++ one.js
@@ -1,20 +1,27 @@
-//classes
-class product {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
+// inheritance = allows a new class to inherit properties and methods from a  existing class
+
+class animal {
+  alive = true;
+  eat() {
+    console.log(`This ${this.name} is eating!!!`);
   }
-  display() {
-    console.log(this.name + " product is priced as : " + this.price);
-  }
-  calcTotal(salesTax) {
-    return this.price + this.price * salesTax;
+  sleep() {
+    console.log(`This ${this.name} is sleeping!!!`);
   }
 }
+class Rabit extends animal {
+  name = "rabit";
+}
+class fish extends animal {
+  name = "fish";
+}
+class hawk extends animal {
+  name = "hawk";
+}
+const hawk1 = new hawk();
+hawk1.eat();
+hawk1.sleep();
 
-const product1 = new product("Laptop", 1000);
-product1.display();
-const salesTax = 0.08;
-
-const total = product1.calcTotal(salesTax);
-console.log(total);
+console.log(hawk1.alive);
+hawk1.alive = false;
+console.log(hawk1.alive);
