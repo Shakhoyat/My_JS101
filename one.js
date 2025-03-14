16e9a7dfe166d6541f657bb5a3491ef9f1e6dedf
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-14 07:23:24 +0600

    prac

--- one.js
+++ one.js
@@ -1,10 +1,28 @@
 //function expression
 
-var one = function () {
-  console.log("oneeeeeee");
-};
-one();
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(function (num) {
+  return num * num;
+});
 
-setTimeout(function () {
-  console.log("timeout after 5 seconds");
-}, 5000);
+console.log(squares);
+//output: [1, 4, 9, 16, 25]
+
+const cubes = numbers.map(function (n) {
+  return n * n * n;
+});
+console.log(cubes);
+//output: [1, 8, 27, 64, 125]
+const evenNum = numbers.filter(function (n) {
+  return n % 2 === 0;
+});
+console.log(evenNum);
+
+//output: [2, 4]
+const totalEven = numbers.reduce(function (acc, n) {
+  if (n % 2 === 0) {
+    return acc + n;
+  }
+  return acc;
+}, 0);
+console.log(totalEven);
