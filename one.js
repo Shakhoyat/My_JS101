c830509ccefe7a5a3b1245b02259bb620899fdb0
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-14 07:33:47 +0600

    arrow functions

--- one.js
+++ one.js
@@ -1,26 +1,13 @@
-//function expression
+//arrow functions
 
-const numbers = [1, 2, 3, 4, 5];
-const squares = numbers.map(function (num) {
-  return num * num;
-});
+const hello = function () {
+  console.log("Hello");
+};
+hello();
 
-console.log(squares);
-//output: [1, 4, 9, 16, 25]
+const hello2 = (nam, age) => {
+  console.log(`Hello ${nam} bro`);
+  console.log(`You are ${age} years old`);
+};
 
-const cubes = numbers.map(function (n) {
-  return n * n * n;
-});
-console.log(cubes);
-//output: [1, 8, 27, 64, 125]
-const evenNum = numbers.filter(function (n) {
-  return n % 2 === 0;
-});
-console.log(evenNum);
-
-//output: [2, 4]
-const total = numbers.reduce(function (accumulator, currentValue) {
-  return accumulator + currentValue;
-});
-console.log(total);
-//output: 15
+hello2("skt", 23);
