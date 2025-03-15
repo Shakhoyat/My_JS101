4b477980aa1acebcbfc65fd4e1a27eb33bd4e5c1
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-15 17:09:31 +0600

    try-catch-finally

--- one.js
+++ one.js
@@ -1,15 +1,9 @@
-//async function to sync function suing callback
-
-function func1(callback) {
-  setTimeout(() => {
-    console.log("task 1 !!!!");
-    callback();
-  }, 3000);
+//try-catch-finally
+try {
+  console.lag("try");
+} catch (e) {
+  console.error(e);
+} finally {
+  console.log("finally");
 }
-function func2() {
-  console.log("task2");
-  console.log("task3");
-  console.log("task4");
-}
-
-func1(func2);
+console.log("You've reached to the end of the code");
