d4a95f1d8b9ece4047a14ff6907b550902baeb4d
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-17 00:23:29 +0600

    getElementsByClassName

--- one.js
+++ one.js
@@ -1,5 +1,18 @@
-//DOM & element selectors
-const myheading = document.getElementById("my-heading");
-myheading.style.color = "red";
-myheading.style.backgroundColor = "black";
-console.log(myheading);
+//html collection
+const fruits = document.getElementsByClassName("fruits");
+
+fruits[0].style.color = "red";
+fruits[1].style.color = "green";
+fruits[2].style.color = "blue";
+
+fruits[0].style.backgroundColor = "black";
+
+// for (let fruit of fruits) {
+//   fruit.style.backgroundColor = "yellow";
+// }
+
+//ForEach() method dont work on html collection so we have to convert it into array
+
+Array.from(fruits).forEach((fruit) => {
+  fruit.style.backgroundColor = "yellow";
+});
