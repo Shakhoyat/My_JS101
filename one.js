a9543c0b05ee7d61f2f31f63111297807bff13af
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-17 00:33:28 +0600

    querrySelector()

--- one.js
+++ one.js
@@ -1,20 +1,5 @@
-//getElement by TagName()
+//querrySelector() is used to select the first element that matches the specified selector(s).
 
-const h4Element = document.getElementsByTagName("h4");
-const liElements = document.getElementsByTagName("li");
+const element = document.querySelector(".fruits");
 
-console.log(h4Element);
-
-h4Element[1].style.color = "red";
-
-for (let i = 0; i < h4Element.length; i++) {
-  h4Element[i].style.backgroundColor = "yellow";
-}
-
-for (let lielement of liElements) {
-  lielement.style.backgroundColor = "lightgreen";
-}
-
-Array.from(liElements).forEach((element) => {
-  element.style.color = "blue";
-});
+element.style.color = "red";
