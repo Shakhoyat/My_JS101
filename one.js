387110357e06c90f04cbcc4827ca9736717fb087
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-17 16:48:41 +0600

    working with li

--- one.js
+++ one.js
@@ -1,27 +1,19 @@
 //step 1 CREATE THE ELEMENT
-const newH1 = document.createElement("h1");
+const newListItem = document.createElement("li");
 
 //step 2 ADD attributes/properties to the  CONTENT
-newH1.textContent = "I like Pizza";
-newH1.id = "pizza";
-newH1.style.color = "tomato";
-newH1.style.fontSize = "50px";
-newH1.style.fontFamily = "Arial";
-newH1.style.textAlign = "center";
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.style.backgroundColor = "lightgreen";
+newListItem.style.fontSize = "20px";
+newListItem.style.fontWeight = "bold";
 
 //step 3 APPEND THE ELEMENT TO THE DOM
-document.getElementById("box4").append(newH1);
-// document.getElementById("box2").prepend(newH1);
-//document.body.appendChild(newH1);
-// document.body.prepend(newH1);
+// document.body.append(newListItem);
+// document.body.prepend(newListItem);
+// document.getElementById("fruits").prepend(newListItem);
 
-// const boxx2 = document.getElementById("box2");
-// document.body.insertBefore(newH1, boxx2);
-
-// const boxes = document.querySelectorAll(".box");
-// document.body.insertBefore(newH1, boxes[4]);
+const orange = document.getElementById("orange");
+document.getElementById("fruits").insertBefore(newListItem, orange);
 
 //remove the element
-// document.body.removeChild(boxes[0]);
-// document.body.removeChild(newH1);
-// document.getElimentById("pizza").removeChild(newH1);
