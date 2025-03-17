83d286dadaab10dbde3d6ce3fdfeedf680cf7de4
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-17 16:27:59 +0600

    Add &change Html

--- one.js
+++ one.js
@@ -1,23 +1,27 @@
-//**************firstelementchild**************
+//step 1 CREATE THE ELEMENT
+const newH1 = document.createElement("h1");
 
-// const fruits = document.getElementById("fruits");
+//step 2 ADD attributes/properties to the  CONTENT
+newH1.textContent = "I like Pizza";
+newH1.id = "pizza";
+newH1.style.color = "tomato";
+newH1.style.fontSize = "50px";
+newH1.style.fontFamily = "Arial";
+newH1.style.textAlign = "center";
 
-// const firstElementChild = fruits.firstElementChild;
+//step 3 APPEND THE ELEMENT TO THE DOM
+document.getElementById("box4").append(newH1);
+// document.getElementById("box2").prepend(newH1);
+//document.body.appendChild(newH1);
+// document.body.prepend(newH1);
 
-// firstElementChild.style.backgroundColor = "yellow";
+// const boxx2 = document.getElementById("box2");
+// document.body.insertBefore(newH1, boxx2);
 
-const ulElements = document.querySelectorAll("ul");
-ulElements.forEach((element) => {
-  const firstChild = element.firstElementChild;
-  if (firstChild) {
-    firstChild.style.backgroundColor = "green";
-  }
-});
+// const boxes = document.querySelectorAll(".box");
+// document.body.insertBefore(newH1, boxes[4]);
 
-//************lastelementchild**********
-
-const lastElementChild = fruits.lastElementChild;
-
-lastElementChild.style.backgroundColor = "pink";
-
-//******nextElementSibling*********** */
+//remove the element
+// document.body.removeChild(boxes[0]);
+// document.body.removeChild(newH1);
+// document.getElimentById("pizza").removeChild(newH1);
