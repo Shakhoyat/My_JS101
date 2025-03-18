eb7dd69e1b939b6a8d318a778ef329b494be3688
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 15:45:44 +0600

    loaded after the DOM content is loaded in the first place

--- one.js
+++ one.js
@@ -4,7 +4,9 @@ const slides = document.querySelectorAll(".slides img");
 let slide_idx = 0;
 let interval_id = null;
 
-initializeSlider();
+// initializeSlider();
+
+document.addEventListener("DOMContentLoaded", initializeSlider); //prefered way to call a function when the page is loaded
 
 function initializeSlider() {
   slides[slide_idx].classList.add("displaySlide");
