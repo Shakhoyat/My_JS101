9e122a9ba392b293a9776f2ff696028f8d9f394b
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 22:33:20 +0600

    Fetching-Pokemon-program

--- one.js
+++ one.js
@@ -1,26 +1,20 @@
-// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data.weight))
-//   .catch((error) => console.error(error));
-
-// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => console.log(data.weight))
-//   .catch((error) => console.error(error));
-
 async function fetchData() {
   try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    const pokemonName = document
+      .getElementById("pokemonName")
+      .value.toLowerCase();
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    console.log(data.weight);
+    const pokemonsprite = data.sprites.front_default;
+    const pokemonImage = document.getElementById("pokemonsprite"); // Updated ID
+
+    pokemonImage.src = pokemonsprite;
+    pokemonImage.style.display = "block";
   } catch (error) {
     console.error(error);
   }
