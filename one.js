8bb7c822e46c7b669714add5453dac9a9e66f804
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 15:02:52 +0600

    myclassList to use a class multiple times

--- one.js
+++ one.js
@@ -1,6 +1,17 @@
 const mybtn = document.getElementById("mybtn");
 
+const myh1 = document.getElementById("myh1");
+
 mybtn.classList.add("enabled");
+myh1.classList.add("enabled");
+
+myh1.addEventListener("click", (event) => {
+  if (event.target.classList.contains("disabled")) {
+    event.target.textContent += ":)";
+  } else {
+    event.target.classList.replace("enabled", "disabled");
+  }
+});
 
 mybtn.addEventListener("click", (event) => {
   if (event.target.classList.contains("disabled")) {
