3211324613e026abe2ec434f53d7dcb7f34a5867
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 16:20:05 +0600

    imageSliding

--- one.js
+++ one.js
@@ -9,9 +9,30 @@ let interval_id = null;
 document.addEventListener("DOMContentLoaded", initializeSlider); //prefered way to call a function when the page is loaded
 
 function initializeSlider() {
-  slides[slide_idx].classList.add("displaySlide");
+  if (slides.length > 0) {
+    slides[slide_idx].classList.add("displaySlide");
+    interval_id = setInterval(nextSlide, 4000);
+  }
+  //   console.log(interval_id);
 }
 
-function showSlide() {}
-function NextSlide() {}
-function PrevSlide() {}
+function showSlide(idx) {
+  if (idx >= slides.length) {
+    slide_idx = 0;
+  } else if (idx < 0) {
+    slide_idx = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slide_idx].classList.add("displaySlide");
+}
+function nextSlide() {
+  slide_idx++;
+  showSlide(slide_idx);
+}
+function prevSlide() {
+  clearInterval(interval_id);
+  slide_idx--;
+  showSlide(slide_idx);
+}
