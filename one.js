904641c838b05817cc282783faf053768c76e26b
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 10:47:41 +0600

    keyevents

--- one.js
+++ one.js
@@ -1,33 +1,4 @@
-//eventListener = Listen for specific events to create interactivity
-//.addEventListener(event,callback)
-
-const mybox = document.getElementById("mybox");
-
-const mybtn = document.getElementById("mybtn");
-// function changeColor(event) {
-//   event.target.style.backgroundColor = "tomato";
-//   event.target.textContent = "ouch!!!";
-// }
-
-// mybox.addEventListener("click", changeColor);
-//or
-// mybox.addEventListener("click", function (event) {
-//   event.target.style.backgroundColor = "tomato";
-//   event.target.textContent = "ouch!!!";
-// });
-
-// OR
-
-mybtn.addEventListener("click", (event) => {
-  event.target.style.backgroundColor = "tomato";
-  event.target.textContent = "ouch!!!";
-});
-
-mybox.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor = "yellow";
-  event.target.textContent = "dont do itttt!!!";
-});
-mybox.addEventListener("mouseout", (event) => {
-  event.target.style.backgroundColor = "lightgreen";
-  event.target.textContent = "oh gosh!!!";
+//keyevents
+document.addEventListener("keydown", (event) => {
+  console.log(event.key);
 });
