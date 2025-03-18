e65a42c24247f3796412f396aafcb6028f4cc03f
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 21:30:20 +0600

    async-await-operaation

--- one.js
+++ one.js
@@ -1,6 +1,28 @@
-const names = ["Alice", "Bob", "Charlie"];
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data.weight))
+//   .catch((error) => console.error(error));
 
-const jsonString = JSON.stringify(names);
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log(data.weight))
+//   .catch((error) => console.error(error));
 
-console.log(jsonString);
-// Output: ["Alice","Bob","Charlie"]
+async function fetchData() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data.weight);
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchData();
