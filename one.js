63a40fe5b91eb01422f638b1c9d75cfeda00fa67
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 11:23:58 +0600

    nodelist with mouseover and mouseout

--- one.js
+++ one.js
@@ -1,8 +1,13 @@
 //NodeLists = static collection of html elements by (id,class,element)
 let buttons = document.querySelectorAll(".mybtn");
 
+//mouseover + mouseout event listener
+
 buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.target.style.backgroundColor = "tomato";
+  button.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "yellow";
+  });
+  button.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = "purple";
   });
 });
