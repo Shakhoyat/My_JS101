a2e5099c2592b41a799fe9ed21a824a09f25a57a
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-18 11:04:36 +0600

    hide/show html

--- one.js
+++ one.js
@@ -1,4 +1,12 @@
-//keyevents
-document.addEventListener("keydown", (event) => {
-  console.log(event.key);
+const mybtn = document.getElementById("mybtn");
+const myimg = document.getElementById("myimg");
+
+mybtn.addEventListener("click", (event) => {
+  if (myimg.style.visibility === "hidden") {
+    myimg.style.visibility = "visible";
+    mybtn.textContent = "Show";
+  } else {
+    myimg.style.visibility = "hidden";
+    mybtn.textContent = "Show";
+  }
 });
